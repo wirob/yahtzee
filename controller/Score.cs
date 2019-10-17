f18e4e0a2c7ee421534387692836cf55d842af74
wirob <c.william.robertson@gmail.com>
2019-10-17 19:48:42 +0200

    Handles the Score

---
+++ controller/Score.cs
@@ -0,0 +1,19 @@
+namespace controller
+{
+  class Score
+  {
+    private int PlayerScore { get; set; }
+
+    private int ComputerScore { get; set; }
+
+    public int GetPlayerScore()
+    {
+      return 13;
+    }
+
+    public int GetComputerScore()
+    {
+      return 11;
+    }
+  }
+}
